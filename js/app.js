2c408c8f5ada991616ab6f69c39be832677ca7d9
nassipkaliv <nassipkaliv@gmail.com>
2025-03-27 12:50:42 +0500

    news-info done

--- js/app.js
+++ js/app.js
@@ -15,21 +15,18 @@ document.addEventListener('DOMContentLoaded', () => {
 document.addEventListener('DOMContentLoaded', () => {
   const backToTopButton = document.querySelector('#top-link');
 
-  // Показываем/скрываем кнопку при прокрутке
   window.addEventListener('scroll', () => {
-    if (window.scrollY > 300) { // Показываем после 300px прокрутки
-      backToTopButton.classList.add('active');
+    if (window.scrollY > 300) { 
     } else {
       backToTopButton.classList.remove('active');
     }
   });
 
-  // Плавная прокрутка наверх при клике
   backToTopButton.addEventListener('click', (e) => {
     e.preventDefault();
     window.scrollTo({
       top: 0,
-      behavior: 'smooth' // Плавная прокрутка
+      behavior: 'smooth' 
     });
   });
 });
